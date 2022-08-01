e6ce9af5cf9891e81d6727c658b0e6a27116cd52
ppPooNnn <pun_yavee@live.com>
2022-08-01 21:19:31 +0700

    little progress

---
+++ ft_strlen.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+size_t ft_strlen (const char *str)
+{
+    size_t len;
+
+    len = 0;
+    while (str != '\0')
+    {
+        str++;
+        len++;
+    }
+    return (len);
+}
--- libft.h
+++ libft.h
@@ -13,5 +13,7 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-
+int ft_isalpha (int c);
+int ft_isdigit (int c);
+size_t  ft_strlen (const char *str);
 #endif
