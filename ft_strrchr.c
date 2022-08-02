a42e727ebe5cae2dc2a115e743a7971fa275bf56
ppPooNnn <pun_yavee@live.com>
2022-08-03 00:42:42 +0700

    add strrchr

---
+++ ft_strrchr.c
@@ -0,0 +1,13 @@
+#include "libft.h"
+
+char    *ft_strrchr (const char *s, int c)
+{
+    int len;
+
+    len = ft_strlen(s);
+    while (len != 0 && s[len] != (char)c)
+        len--;
+    if (s[len] == (char)c)
+        return ((char *)c);
+    return (0);
+}
--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ int     ft_isascii (int c);
 int     ft_isprint (int c);
 int     ft_toupper (int c);
 int     ft_tolower (int c);
-char    *strchr (const char *s, int c);
+char    *ft_strchr (const char *s, int c);
+char    *ft_strrchr (const char *s, int c);
 size_t  ft_strlen (const char *str);
 #endif
