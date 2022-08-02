eae2052d93ca42033b6e4f83a371cb0c306fc63f
ppPooNnn <pun_yavee@live.com>
2022-08-02 23:40:44 +0700

    edit strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -5,6 +5,8 @@ char    *ft_strchr (const char *s, int c)
     int count;
 
     count = 0;
+    if (s[count] == '\0')
+        return ((char *) s);
     while (s[count] != '\0')
     {
         if (s[count] == (char)c)
