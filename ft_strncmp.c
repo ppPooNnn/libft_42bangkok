09fd45ea4a679a5a27a839e7ae2ece142c5ceb0f
ppPooNnn <pun_yavee@live.com>
2022-08-03 02:41:31 +0700

    edit strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@ int ft_strncmp (const char *s1, const char *s2, size_t n)
     int check;
 
     count = 0;
-    while (count <= n && (s1[count] || s2[count]))
+    while (count < n && (s1[count] || s2[count]))
     {
         if (s1[count] != s2[count])
         {
