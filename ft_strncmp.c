96df625fda3fe72d5c0e7ef022a30424985089f6
ppPooNnn <pun_yavee@live.com>
2022-08-03 01:44:40 +0700

    edit strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,13 +2,15 @@
 
 int ft_strncmp (const char *s1, const char *s2, size_t n)
 {
-    int count;
+    size_t count;
 
     count = 0;
-    while (count <= n)
+    while (count <= n && *s1 != '\0' && s2 != '\0')
     {
         if (*s1 != *s2)
-            return (*s1 - *s2);
+            return ((const char *)s1 - (const char *)s2);
+        s1++;
+        s2++;
         count++;
     }
     return (0);
