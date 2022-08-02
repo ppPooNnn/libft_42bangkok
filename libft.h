84fc433ceb5915975a2d89edb3c7773b5498e47f
ppPooNnn <pun_yavee@live.com>
2022-08-02 23:21:57 +0700

    add more header

--- libft.h
+++ libft.h
@@ -17,10 +17,13 @@
 #include <unistd.h>
 
 
-int ft_isalpha (int c);
-int ft_isdigit (int c);
-int ft_isalnum (int c);
-int ft_isascii (int c);
-int ft_isprint (int c);
+int     ft_isalpha (int c);
+int     ft_isdigit (int c);
+int     ft_isalnum (int c);
+int     ft_isascii (int c);
+int     ft_isprint (int c);
+int     ft_toupper (int c);
+int     ft_tolower (int c);
+char    *strchr (const char *s, int c);
 size_t  ft_strlen (const char *str);
 #endif
