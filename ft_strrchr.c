0829eca64f434189a183bd8db54b4f457f3a1cea
ppPooNnn <pun_yavee@live.com>
2022-08-03 01:09:36 +0700

    edit strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,9 +5,10 @@ char    *ft_strrchr (const char *s, int c)
     int len;
 
     len = ft_strlen(s);
-    while (len != 0 && s[len] != (char)c)
-        len--;
-    if (s[len] == (char)c)
-        return ((char *)s);
+    while (len >= 0)
+    {
+        if (s[len] == (char)c)
+            return ((char *)s);
+    }
     return (0);
 }
