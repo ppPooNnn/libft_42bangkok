29a8ca20ae7261f6aea7aa8da7cf1eb7ebb2ad77
ppPooNnn <pun_yavee@live.com>
2022-08-03 03:01:59 +0700

    edit memset

--- ft_memset.c
+++ ft_memset.c
@@ -5,9 +5,10 @@ void	*ft_memset (void *s, int c, size_t n)
 	size_t	count;
 
 	count = 0;
-	while (count <= n)
+	while (count < n)
 	{
-		s[count] = (char)c;
+		(char *)(s + count) = (char)c;
 		count++;
 	}
+	return (s);
 }
