3f27967f1709db16c1c3843337105d7eac615b74
ppPooNnn <pun_yavee@live.com>
2022-08-03 23:43:10 +0700

    add memcpy

---
+++ ft_memcpy.c
@@ -0,0 +1,12 @@
+#include "libft.h"
+
+void	*ft_memcpy (void *restrict dest, const void *restrict src, size_t n)
+{
+	size_t	count;
+
+	count = 0;
+	while (count < n)
+	{
+		(char *) dest + count = (char *) src + count;
+	}
+}
--- libft.h
+++ libft.h
@@ -29,6 +29,7 @@ char	*ft_strchr (const char *s, int c);
 char	*ft_strrchr (const char *s, int c);
 void	*ft_memset (void *s, int c, size_t n);
 void	*ft_memchr (const void *s, int c, size_t n);
+void	*ft_memcpy (void *restrict dest, const void *restrict src, size_t n);
 void	ft_bzero (void *s, size_t n);
 size_t	ft_strlen (const char *str);
 #endif
