416361cd36108535fdedc1c838c95f583cff93ba
ppPooNnn <pun_yavee@live.com>
2022-08-04 20:28:19 +0700

    edit memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -19,11 +19,11 @@ int	ft_memcmp (const void *s1, const void *s2, size_t n)
 
 	check = 0;
 	count = 0;
-	while (count < n && ((char *)s1[count] || (char *)s2[count]))
+	while (count < n && (((char *)s1)[count] || ((char *)s2)[count]))
 	{
-		if ((unsigned char *)s1[count] != (unsigned char *)s2[count])
+		if (((unsigned char *)s1)[count] != ((unsigned char *)s2)[count])
 		{
-			check = (unsigned char *)s1[count] - (unsigned char *)s2[count];
+			check = ((unsigned char *)s1)[count] - ((unsigned char *)s2)[count];
 			if (check > 0)
 				return (1);
 			else if (check < 0)
