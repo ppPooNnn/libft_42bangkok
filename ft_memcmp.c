75daa69b3ae4e7b1d04fbf67f1a6219e2ec178a0
Pradipas Uttasa <puttasa@z3t4c3.42bangkok.com>
2022-08-05 18:10:45 +0700

    edit memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: puttasa <puttasa@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 00:25:24 by puttasa           #+#    #+#             */
-/*   Updated: 2022/08/04 00:25:24 by puttasa          ###   ########.fr       */
+/*   Updated: 2022/08/05 18:09:44 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,14 @@ int	ft_memcmp (const void *s1, const void *s2, size_t n)
 
 	check = 0;
 	count = 0;
-	while (count < n && (((char *)s1)[count] || ((char *)s2)[count]))
+	if (n == 0)
+		return (0);
+	while (count < n)
 	{
 		if (((unsigned char *)s1)[count] != ((unsigned char *)s2)[count])
 		{
 			check = ((unsigned char *)s1)[count] - ((unsigned char *)s2)[count];
-			if (check > 0)
-				return (1);
-			else if (check < 0)
-				return (-1);
-			else
-				return (0);
+			return (check);
 		}
 		count++;
 	}
