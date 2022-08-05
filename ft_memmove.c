678635c56e52d15d4e2b9ec9fba2d404d959e3bd
Pradipas Uttasa <puttasa@z1t2c2.42bangkok.com>
2022-08-06 04:21:36 +0700

    add memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,34 @@
 /*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 18:16:33 by puttasa           #+#    #+#             */
-/*   Updated: 2022/08/05 22:31:06 by puttasa          ###   ########.fr       */
+/*   Updated: 2022/08/06 04:19:49 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	int	c;
 
+	c = 0;
+	if (!(dest || src))
+		return (dest);
+	if (dest > src)
+	{
+		while (n > 0)
+		{
+			*((unsigned char *)dest + n) = *((unsigned char *)src + n);
+			n--;
+		}
+	}
+	else
+	{
+		while (n > 0)
+		{
+			*((unsigned char *)dest + c) = *((unsigned char *)src + c);
+			c++;
+		}
+	}
+	return (dest);
 }
