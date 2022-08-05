54b4707d161444b59e3765838da3b61cea19062d
Pradipas Uttasa <puttasa@z1t2c2.42bangkok.com>
2022-08-06 04:21:52 +0700

    add memmove

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: puttasa <puttasa@student.42bangkok.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 00:16:46 by puttasa           #+#    #+#             */
-/*   Updated: 2022/08/06 03:53:09 by puttasa          ###   ########.fr       */
+/*   Updated: 2022/08/06 03:58:25 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 void	ft_bzero(void *s, size_t n);
 size_t	ft_strlen(const char *str);
