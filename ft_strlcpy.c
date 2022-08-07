63a29919a7e1d25532745be8ceaef9000875349a
Pradipas Uttasa <puttasa@z2t10c3.42bangkok.com>
2022-08-07 13:08:15 +0700

    edit strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -5,10 +5,10 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	count;
 
 	count = 0;
-	while (count < n && (dst || src))
+	while (count < size && (dst || src))
 	{
-		*(char *)(dest + count) = *(char *)(src + count);
+		*(char *)(dst + count) = *(char *)(src + count);
 		count++;
 	}
-	return (dst);
+	return (ft_strlen(dst));
 }
