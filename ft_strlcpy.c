05c28af5bb1b73decc05e3726654533a990459fa
Pradipas Uttasa <puttasa@z2t10c3.42bangkok.com>
2022-08-11 20:30:51 +0700

    edit strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/11 20:26:19 by puttasa           #+#    #+#             */
+/*   Updated: 2022/08/11 20:30:24 by puttasa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
@@ -5,7 +17,9 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	count;
 
 	count = 0;
-	while (count < size && (dst || src))
+	if (!(dst && src))
+		return (0);
+	while (count < size - 1 && (dst || src))
 	{
 		*(char *)(dst + count) = *(char *)(src + count);
 		count++;
