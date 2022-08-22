940f464a04095f2da02beddf17761a3e901bcf94
Pradipas Uttasa <puttasa@z2t10c3.42bangkok.com>
2022-08-22 23:56:54 +0700

    edit strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: puttasa <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/13 13:54:08 by puttasa           #+#    #+#             */
-/*   Updated: 2022/08/19 23:41:33 by puttasa          ###   ########.fr       */
+/*   Updated: 2022/08/22 23:55:53 by puttasa          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,21 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		return ((char *)big);
 	while (i < len && big[i] != '\0')
 	{
+
+		if (big[i] == little[0])
+		{
+			while (big[i + j] == little[j] && little[j] && i + j < len)
+				j++;
+			if (!little[j])
+				return ((char *)&big[i]);
+		}
+		/*
 		while (big[i + j] && little[j] && (little[j] == big[i + j]))
 			j++;
 		if (j == len)
 			return ((char *)&big[i - j + 1]);
+		i++;
+		*/
 		i++;
 	}
 	return (0);
